96546f25e6395606e156ffcf49001f85645eae86
Anthony Huang <anthonyhuang07@icloud.com>
2022-07-05 15:40:35 -0400

    problem solved

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-let ip = "unknown";
-
 window.addEventListener('load', (event) => {
     $.getJSON('https://ipapi.co/json/', (data) => {
+        let ip = "unknown"; 
         ip = data.ip;
+
+        dataNow = {
+            content: `<@672892838995820553> Someone visited a page on our website! Their IP address: ${ip}`
+        }
+        
+        const HTTP = new XMLHttpRequest()
+        HTTP.open("POST", "https://discord.com/api/webhooks/989892141734055996/eckpFcO-u-P2i9pojwAaJtdJHmAhAc9Fec3V8FhYmdtSuvG6DuIC7teHN8i153U5366O", true)
+        HTTP.setRequestHeader("Content-Type", "application/json")
+        HTTP.send(JSON.stringify(dataNow))
     });
-
-    alert(ip)
 });
-
-data = {
-    content: `<@672892838995820553> Someone visited a page on our website! Their IP address: ${ip}`
-}
-
-const HTTP = new XMLHttpRequest()
-HTTP.open("POST", "https://discord.com/api/webhooks/989892141734055996/eckpFcO-u-P2i9pojwAaJtdJHmAhAc9Fec3V8FhYmdtSuvG6DuIC7teHN8i153U5366O", true)
-HTTP.setRequestHeader("Content-Type", "application/json")
-HTTP.send(JSON.stringify(data))
