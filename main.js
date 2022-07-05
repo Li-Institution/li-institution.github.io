9b68085b849a1acb4f61e67bc9dd7fa448b3b0b3
carpetmaker3162 <82886347+carpetmaker3162@users.noreply.github.com>
2022-07-05 16:16:07 -0400

    change webhook

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.addEventListener('load', (event) => {
         }
         
         const HTTP = new XMLHttpRequest()
-        HTTP.open("POST", "https://discord.com/api/webhooks/989892141734055996/eckpFcO-u-P2i9pojwAaJtdJHmAhAc9Fec3V8FhYmdtSuvG6DuIC7teHN8i153U5366O", true)
+        HTTP.open("POST", "https://discord.com/api/webhooks/993973447040434360/c9KNCteozfPTUXXFZe42-qIg9Ik5eQNPlcZnA1-ynI6MvDOIKv7pK4mLaoSTvgRpZ3jK", true)
         HTTP.setRequestHeader("Content-Type", "application/json")
         HTTP.send(JSON.stringify(dataNow))
     });
